6159a6de82d441f356e5a8f7696cb5c1e01cd96a
nicotonin <nicotonin05@gmail.com>
2026-05-25 18:47:51 +0200

    prova n1 per deply

--- back-front-base/frontend/src/app/service/user.service.ts
+++ back-front-base/frontend/src/app/service/user.service.ts
@@ -2,8 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { User } from '../entities/user.entity';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
